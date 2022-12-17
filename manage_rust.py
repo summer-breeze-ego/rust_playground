3f40fe4c8eb85a811442ff4746c5c7bafdae89ea
Augustin <103153576+summer-breeze-ego@users.noreply.github.com>
2022-12-17 04:44:32 +0100

    new rust project

---
+++ manage_rust.py
@@ -0,0 +1,9 @@
+import os
+
+RUST_PACKAGE = 'hello_world'
+
+# creating new tree
+os.system(f"cargo new {RUST_PACKAGE}")
+
+# moving
+os.system(f"cargo run --manifest-path ./hello_world/Cargo.toml")
